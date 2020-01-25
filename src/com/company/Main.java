e2023a03bbf51f4cac4620df240f43d4bb2bee37
Patryk Świerkowski <phemistrily@gmail.com>
2020-01-25 14:31:11 +0100

    change for socket

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyServer server = new MyServer();
+    }
+}
